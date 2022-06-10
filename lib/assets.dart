397f76cef4284ace6c42650e7474c39f04d8541e
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-06-10 22:12:55 +0200

    Implementati gli assets.

---
+++ lib/assets.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class Images {
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/card.png)
+  static AssetImage get card => const AssetImage("assets/card.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/logo.png)
+  static AssetImage get logo => const AssetImage("assets/logo.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/receipt1.png)
+  static AssetImage get receipt1 => const AssetImage("assets/receipt1.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/receipt2.png)
+  static AssetImage get receipt2 => const AssetImage("assets/receipt2.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/receipt3.png)
+  static AssetImage get receipt3 => const AssetImage("assets/receipt3.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/scontree/assets/receipt4.png)
+  static AssetImage get receipt4 => const AssetImage("assets/receipt4.png");
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Scontree());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Scontree extends StatelessWidget {
+  const Scontree({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
