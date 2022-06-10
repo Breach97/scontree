4b20c05621221dfe10c363aaedd2e975ae9eebf9
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-06-10 23:38:51 +0200

    Implementato il deploy per Firebase.

--- lib/interface/screen/insert_card_screen.dart
+++ lib/interface/screen/insert_card_screen.dart
@@ -16,44 +16,41 @@ class InsertCardScreen extends StatelessWidget {
 
   Widget _buildBody(BuildContext context) {
     return SafeArea(
-      child: Padding(
+      child: ListView(
         padding: const EdgeInsets.all(32.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FractionallySizedBox(widthFactor: 3 / 4, child: Image(image: Images.logo)),
-            Flexible(child: SizedBox(height: 16.0)),
-            GradientText("Associa la tua carta e ricevi gli Scontree quando paghi!",
-                gradient: References.gradient, style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center),
-            Spacer(),
-            ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(References.generalRadius / 2)),
-              child: Image(
-                image: Images.card,
-              ),
+        children: [
+          FractionallySizedBox(widthFactor: 3 / 4, child: Image(image: Images.logo)),
+          Flexible(child: SizedBox(height: 16.0)),
+          GradientText("Associa la tua carta e ricevi gli Scontree quando paghi!",
+              gradient: References.gradient, style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center),
+          Spacer(),
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(References.generalRadius / 2)),
+            child: Image(
+              image: Images.card,
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "---- ---- ---- ----",
-                labelText: "Numero carta",
-              ),
-              readOnly: true,
-              enabled: false,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "---- ---- ---- ----",
+              labelText: "Numero carta",
             ),
-            SizedBox(height: 16.0),
-            Text(
-              "Nessun dato inserito verrà salvato ma solo processato al fine di garantire il servizio, per maggiori informazioni leggere i termini e le condizioni",
-              style: Theme.of(context).textTheme.caption,
-            ),
-            Spacer(),
-            ElevatedButton(
-              child: Text("Registrati"),
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed(HomeScreen.route);
-              },
-            ),
-          ],
-        ),
+            readOnly: true,
+            enabled: false,
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            "Nessun dato inserito verrà salvato ma solo processato al fine di garantire il servizio, per maggiori informazioni leggere i termini e le condizioni",
+            style: Theme.of(context).textTheme.caption,
+          ),
+          Spacer(),
+          ElevatedButton(
+            child: Text("Registrati"),
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed(HomeScreen.route);
+            },
+          ),
+        ],
       ),
     );
   }
